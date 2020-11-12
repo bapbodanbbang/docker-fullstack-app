9f68f230d6595b8e9cb02f106ac33783e4b40043
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-11-12 23:31:02 +0900

    make fullstack docker react app

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fields) => {
+    console.log('results : ',results);
+})
+
+app.get('/api/values', (req,res) => {
+    db.pool.query('SELECT * FROM lists;',
+    (err, results, fields) => {
+        if(err) return res.status(500).send(err)
+        else res.json(results);
+    })
+})
+
+app.post('/api/value', (req,res) => {
+    db.pool.query(`INSERT INTO lists (value) VALUES(${req.body.value})`,
+    (err,results) => {
+        if(err) return res.status(500).send(err)
+        else return res.json({ success : true, value : req.body.value })
+    })
+})
+
+app.listen(5000, () => {
+    console.log('5000번 포트에서 시작합니다!')
+})
